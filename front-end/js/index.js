0da3712c27cb335c66c2a9d923c4979cc295845d
Brian Rouyre <brian.rouyre@hotmail.com>
2021-05-06 20:04:40 +0200

    push front-end

---
+++ front-end/js/index.js
@@ -0,0 +1,26 @@
+
+let url = 'http://localhost:3000/api/teddies';
+
+fetch( url , {method : 'GET'})
+.then(data => {
+    return data.json()
+}).then(products => {
+    console.log(products)
+
+    let HTML = document.getElementById("produit")
+
+    let myHTML = ""
+    products.forEach(product => {
+        console.log(product.name)
+        console.log(product.price)
+        myHTML += `<div class="card">
+                        <figure><img src=${product.imageUrl} alt="${product.name}"></figure>
+                        <p>${product.name}</p>
+                        <p>${product.price}</p>
+                        <input type="button" value="Voir le produit">
+                    </div>`
+    });
+
+    console.log(myHTML)
+    HTML.innerHTML = myHTML
+})
